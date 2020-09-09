318184ef0fb0dbfe14ed254a095d6a5503361197
rishabhk001 <47914617+rishabhk001@users.noreply.github.com>
2020-09-09 22:00:49 +0530

    Add files via upload

---
+++ static/dashboard/script.js
@@ -0,0 +1,39 @@
+// JavaScript source code
+function active_tilep() {
+    var active = document.createElement("style");
+    //active.style.background = "#0d1623";
+    //active.style.borderLeft = "5px solid #b03232";
+    //active.style.padding = "15px 30px 15px 25px";
+    active.innerHTML="#profile{background:#0d1623; border-left:5px solid #b03232;padding:15px 30px 15px 25px;}#profile>i{color:#b03232;}";
+    document.body.append(active);
+}
+function active_tileh() {
+    var active = document.getElementById("home");
+    active.style.background = "#0d1623";
+    active.style.borderLeft = "5px solid #b03232";
+    active.style.padding = "15px 30px 15px 25px";
+}
+function active_tiler() {
+    var active = document.getElementById("report");
+    active.style.background = "#0d1623";
+    active.style.borderLeft = "5px solid #b03232";
+    active.style.padding = "15px 30px 15px 25px";
+}
+function active_tilet() {
+    var active = document.getElementById("tests");
+    active.style.background = "#0d1623";
+    active.style.borderLeft = "5px solid #b03232";
+    active.style.padding = "15px 30px 15px 25px";
+}
+function active_tileu() {
+    var active = document.getElementById("up_tests");
+    active.style.background = "#0d1623";
+    active.style.borderLeft = "5px solid #b03232";
+    active.style.padding = "15px 30px 15px 25px";
+}
+function active_tilec() {
+    var active = document.getElementById("contact");
+    active.style.background = "#0d1623";
+    active.style.borderLeft = "5px solid #b03232";
+    active.style.padding = "15px 30px 15px 25px";
+}
